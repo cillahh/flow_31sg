8dbc427f02312ecd6f7e5e2d52960764df8d347e
cillahh <cillah164@naver.com>
2025-11-14 19:52:20 +0900

    fix: vecel update test

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -147,7 +147,7 @@ class _HomeScreenState extends State<HomeScreen> {
               style: Theme.of(context).textTheme.titleMedium?.copyWith(
                 fontFamily: 'HelveticaRounded',
                 fontSize: 25 * Util.getScaleHeight(context), // [수정]
-                color: kPrimaryColor,
+                color: Colors.brown,
                 fontWeight: FontWeight.w900,
               ),
             ),
